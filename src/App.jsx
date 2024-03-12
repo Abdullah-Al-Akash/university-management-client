b59bcf1276e61b7d70bba59909bd583edb13fb0b
ashiqur-r-biplop <biplops22@gmail.com>
2024-03-12 12:53:58 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+import React from "react";
+import Select from "./Components/Select";
+
+const App = () => {
+  return (
+    <div>
+      <Select />
+    </div>
+  );
+};
+
+export default App;

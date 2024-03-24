4246eac24b30ccffdb1e39edddf153844ebab5f1
ashiqur-r-biplop <biplops22@gmail.com>
2024-03-24 11:43:06 +0600

    git push

---
+++ src/Shared/TableWrapper.jsx
@@ -0,0 +1,17 @@
+/* eslint-disable react/prop-types */
+const TableWrapper = ({ children, minWidth, borderStyle }) => {
+    return (
+      <section
+        className={`overflow-x-auto custom-table-class rounded-[6px] ${
+          borderStyle ? borderStyle : "border"
+        }`}
+      >
+        <div className={` ${minWidth ? minWidth : "min-w-[1000px]"}`}>
+          {children}
+        </div>
+      </section>
+    );
+  };
+  
+  export default TableWrapper;
+  

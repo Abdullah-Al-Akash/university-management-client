7c7c8665a74c7e907598a1bf46af87c740f43189
ashiqur-r-biplop <biplops22@gmail.com>
2024-03-26 15:46:31 +0600

    added modal

--- src/Components/Regular/RegularTable.jsx
+++ src/Components/Regular/RegularTable.jsx
@@ -1,8 +1,8 @@
 /* eslint-disable react/prop-types */
 import TableWrapper from "../../Shared/TableWrapper";
+import TeacherAssign from "../Modal/TeacherAssign";
 
 const RegularTable = ({ data, loading }) => {
-
   return (
     <TableWrapper>
       <table
@@ -117,74 +117,158 @@ const RegularTable = ({ data, loading }) => {
                   >
                     {room}
                   </td>
-                  <td
-                    colSpan={3}
-                    className={`px-[16px] border-r-[1px] py-[6px] text-[#000] border-[#000] border-2 text-[14px]`}
-                  >
-                    {courses["0"]?.["1"]?.courseCode && (
-                      <>
-                        {courses["0"]?.["1"]?.courseCode}{" "}
-                        {courses["0"]?.["1"]?.courseTitle}{" "}
-                      </>
-                    )}
-                  </td>
-                  <td
-                    colSpan={3}
-                    className={`px-[16px] border-r-[1px] py-[6px] text-[#000] border-[#000] border-2 text-[14px]`}
-                  >
-                    {courses["1"]?.["2"]?.courseCode && (
-                      <>
-                        {courses["1"]?.["2"]?.courseCode}{" "}
-                        {courses["1"]?.["2"]?.courseTitle}{" "}
-                      </>
-                    )}
-                  </td>
-                  <td
-                  colSpan={
-                    courses["2"]?.["3"]?.courseTitle?.includes("Sessional") ? 2 : 1
-                  }
-                    className={`px-[16px] border-r-[0px] py-[6px] text-[#000] border-[#000] border-2 text-[14px] ${
-                      courses?.["3"]?.courseTitle?.includes("Sessional")
-                        ? "border-r-0"
-                        : ""
-                    }`}
-                  >
-                    {courses["2"]?.["3"]?.courseCode && (
-                      <>
-                        {courses["2"]?.["3"]?.courseCode}{" "}
-                        {courses["2"]?.["3"]?.courseTitle}{" "}
-                      </>
-                    )}
-                  </td>
-                  {
-                    !courses["2"]?.["3"]?.courseTitle?.includes("Sessional") && (<td
+                  {courses["0"]?.["1"]?.courseTitle ? (
+                    <td
+                      colSpan={3}
+                      onClick={() =>
+                        document.getElementById("teacher_assign").showModal()
+                      }
+                      className={`px-[16px] border-r-[1px] py-[6px] text-[#000] border-[#000] border-2 text-[14px] cursor-pointer`}
+                    >
+                      {courses["0"]?.["1"]?.courseCode && (
+                        <>
+                          {courses["0"]?.["1"]?.courseCode}{" "}
+                          {courses["0"]?.["1"]?.courseTitle}{" "}
+                        </>
+                      )}
+                    </td>
+                  ) : (
+                    <td
+                      colSpan={3}
+                      className={`px-[16px] border-r-[1px] py-[6px] text-[#000] border-[#000] border-2 text-[14px]`}
+                    >
+                      {courses["0"]?.["1"]?.courseCode && (
+                        <>
+                          {courses["0"]?.["1"]?.courseCode}{" "}
+                          {courses["0"]?.["1"]?.courseTitle}{" "}
+                        </>
+                      )}
+                    </td>
+                  )}
+                  {courses["1"]?.["2"]?.courseTitle ? (
+                    <td
+                      colSpan={3}
+                      onClick={() =>
+                        document.getElementById("teacher_assign").showModal()
+                      }
+                      className={`px-[16px] border-r-[1px] py-[6px] text-[#000] border-[#000] border-2 text-[14px] cursor-pointer`}
+                    >
+                      {courses["1"]?.["2"]?.courseCode && (
+                        <>
+                          {courses["1"]?.["2"]?.courseCode}{" "}
+                          {courses["1"]?.["2"]?.courseTitle}{" "}
+                        </>
+                      )}
+                    </td>
+                  ) : (
+                    <td
+                      colSpan={3}
+                      className={`px-[16px] border-r-[1px] py-[6px] text-[#000] border-[#000] border-2 text-[14px]`}
+                    >
+                      {courses["1"]?.["2"]?.courseCode && (
+                        <>
+                          {courses["1"]?.["2"]?.courseCode}{" "}
+                          {courses["1"]?.["2"]?.courseTitle}{" "}
+                        </>
+                      )}
+                    </td>
+                  )}
+                  {courses["2"]?.["3"]?.courseTitle ? (
+                    <td
+                      colSpan={
+                        courses["2"]?.["3"]?.courseTitle?.includes("Sessional")
+                          ? 2
+                          : 1
+                      }
+                      onClick={() =>
+                        document.getElementById("teacher_assign").showModal()
+                      }
+                      className={`px-[16px] border-r-[0px] py-[6px] text-[#000] border-[#000] border-2 text-[14px] cursor-pointer ${
+                        courses?.["3"]?.courseTitle?.includes("Sessional")
+                          ? "border-r-0"
+                          : ""
+                      }`}
+                    >
+                      {courses["2"]?.["3"]?.courseCode && (
+                        <>
+                          {courses["2"]?.["3"]?.courseCode}{" "}
+                          {courses["2"]?.["3"]?.courseTitle}{" "}
+                        </>
+                      )}
+                    </td>
+                  ) : (
+                    <td
+                      colSpan={
+                        courses["2"]?.["3"]?.courseTitle?.includes("Sessional")
+                          ? 2
+                          : 1
+                      }
+                      className={`px-[16px] border-r-[0px] py-[6px] text-[#000] border-[#000] border-2 text-[14px] ${
+                        courses?.["3"]?.courseTitle?.includes("Sessional")
+                          ? "border-r-0"
+                          : ""
+                      }`}
+                    >
+                      {courses["2"]?.["3"]?.courseCode && (
+                        <>
+                          {courses["2"]?.["3"]?.courseCode}{" "}
+                          {courses["2"]?.["3"]?.courseTitle}{" "}
+                        </>
+                      )}
+                    </td>
+                  )}
+                  {!courses["2"]?.["3"]?.courseTitle?.includes("Sessional") && (
+                    <td
                       className={`px-[16px] border-r-[1px] py-[6px] text-[#000] border-[#000] border-2 text-[14px] ${
                         courses["3"]?.["3"]?.courseTitle?.includes("Sessional")
                           ? "border-l-0"
                           : ""
                       }`}
-                    ></td>)
-                  }
-                  
+                    ></td>
+                  )}
+
                   <td className="px-[16px] border-r-[1px] py-[6px] text-[#000] border-[#000] border-2 border-t-0 border-b-0 text-[20px]">
                     {" "}
                     {index === 4 ? "B" : ""} {index === 5 ? "R" : ""}{" "}
                     {index === 6 ? "E" : ""} {index === 7 ? "A" : ""}{" "}
                     {index === 8 ? "K" : ""}
                   </td>
-                  <td
-                    className={`px-[16px]  py-[6px] text-[#000] border-[#000] border-2 text-[14px]`}
-                    colSpan={
-                      courses["3"]?.["4"]?.courseTitle?.includes("Sessional") ? 2 : 1
-                    }
-                  >
-                    {courses["3"]?.["4"]?.courseCode && (
-                      <>
-                        {courses["3"]?.["4"]?.courseCode}{" "}
-                        {courses["3"]?.["4"]?.courseTitle}{" "}
-                      </>
-                    )}
-                  </td>
+                  {courses["3"]?.["4"]?.courseTitle ? (
+                    <td
+                      onClick={() =>
+                        document.getElementById("teacher_assign").showModal()
+                      }
+                      className={`px-[16px]  py-[6px] text-[#000] border-[#000] border-2 text-[14px] cursor-pointer`}
+                      colSpan={
+                        courses["3"]?.["4"]?.courseTitle?.includes("Sessional")
+                          ? 2
+                          : 1
+                      }
+                    >
+                      {courses["3"]?.["4"]?.courseCode && (
+                        <>
+                          {courses["3"]?.["4"]?.courseCode}{" "}
+                          {courses["3"]?.["4"]?.courseTitle}{" "}
+                        </>
+                      )}
+                    </td>
+                  ) : (
+                    <td
+                      className={`px-[16px]  py-[6px] text-[#000] border-[#000] border-2 text-[14px]`}
+                      colSpan={
+                        courses["3"]?.["4"]?.courseTitle?.includes("Sessional")
+                          ? 2
+                          : 1
+                      }
+                    >
+                      {courses["3"]?.["4"]?.courseCode && (
+                        <>
+                          {courses["3"]?.["4"]?.courseCode}{" "}
+                          {courses["3"]?.["4"]?.courseTitle}{" "}
+                        </>
+                      )}
+                    </td>
+                  )}
                   {!courses["3"]?.["4"]?.courseTitle?.includes("Sessional") && (
                     <td
                       className={`px-[16px]  py-[6px] text-[#000] border-[#000] border-2 text-[14px]`}
@@ -194,6 +278,7 @@ const RegularTable = ({ data, loading }) => {
               );
             })}
       </table>
+      <TeacherAssign />
     </TableWrapper>
   );
 };

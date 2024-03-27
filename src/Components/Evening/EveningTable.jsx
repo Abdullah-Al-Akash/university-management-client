286c747b625657fb225a9e3e9737a2b655241b82
Source-Code-007 <utsho926@gmail.com>
2024-03-27 10:12:04 +0600

    teacher assign functionality added for evening table

--- src/Components/Evening/EveningTable.jsx
+++ src/Components/Evening/EveningTable.jsx
@@ -1,6 +1,10 @@
+import { useState } from 'react';
 import TableWrapper from '../../Shared/TableWrapper';
+import TeacherAssign from '../Modal/TeacherAssign';
 
 const EveningTable = ({ data, loading }) => {
+    const [courseId, setCourseId] = useState("");
+    const [rowIndex, setRowIndex] = useState(null);
     console.log(data, 'from evening table');
     return (
         <TableWrapper>
@@ -30,6 +34,7 @@ const EveningTable = ({ data, loading }) => {
                             const { batch, yearSem, sem, room, courses } = elem
                             const classesBeforeBreak = courses.slice(0, 2)
                             const classesAfterBreak = courses.slice(2, 4)
+
                             // console.log(elem);
                             return <tr key={ind} >
                                 <td className='border border-black p-3'>{batch}</td>
@@ -37,18 +42,50 @@ const EveningTable = ({ data, loading }) => {
                                 <td className='border border-black p-3'>{sem}</td>
                                 <td className='border border-black p-3'>{room}</td>
                                 {
-                                    classesBeforeBreak[0]?.courseCode === classesBeforeBreak[1]?.courseCode ? <td className={`border border-black p-3`} colSpan={2}>
+                                    classesBeforeBreak[0]?.courseCode === classesBeforeBreak[1]?.courseCode ? <td
+                                        onClick={() => {
+                                            if (classesBeforeBreak[0]?.courseCode || classesBeforeBreak[0]?.courseTitle) {
+                                                setCourseId(classesBeforeBreak[0]?._id);
+                                                setRowIndex(classesBeforeBreak[0]?.rowIndex);
+                                                document.getElementById("teacher_assign").showModal();
+                                            }
+                                        }}
+                                        className={`border border-black p-3`} colSpan={2}>
                                         {`${classesBeforeBreak[0]?.courseCode ?? ''} ${classesBeforeBreak[0]?.courseTitle ? `(${classesBeforeBreak[0]?.courseTitle})` : ''} ${classesBeforeBreak[0]?.teacher?.sortForm ?? ''}`}
-                                    </td> : classesBeforeBreak?.map((elem, ind) => <td key={ind} className={`border border-black p-3`}>
+                                    </td> : classesBeforeBreak?.map((elem, ind) => <td key={ind}
+                                        onClick={() => {
+                                            if (elem?.courseCode || elem?.courseTitle) {
+                                                setCourseId(elem?._id);
+                                                setRowIndex(elem?.rowIndex);
+                                                document.getElementById("teacher_assign").showModal();
+                                            }
+                                        }}
+                                        className={`border border-black p-3`}>
                                         {`${elem?.courseCode ?? ''} ${elem?.courseTitle ? `(${elem?.courseTitle})` : ''}  ${elem?.teacher?.sortForm ?? ''}`}
                                     </td>
                                     )
                                 }
                                 {ind === 0 && <td rowSpan={data.length}><div className='flex flex-col font-bold'>{"BREAK".split('').map((elem, ind) => <span key={ind}>{elem}</span>)}</div></td>}
                                 {
-                                    classesAfterBreak[0]?.courseCode === classesAfterBreak[1]?.courseCode ? <td className={`border border-black p-3`} colSpan={2}>
-                                        {`${classesBeforeBreak[0]?.courseCode ?? ''} ${classesBeforeBreak[0]?.courseTitle ? `(${classesBeforeBreak[0]?.courseTitle})` : ''} ${classesBeforeBreak[0]?.teacher?.sortForm ?? ''}`}
-                                    </td> : classesAfterBreak?.map((elem, ind) => <td key={ind} className={`border border-black p-3`}>
+                                    classesAfterBreak[0]?.courseCode === classesAfterBreak[1]?.courseCode ? <td
+                                        onClick={() => {
+                                            if (classesAfterBreak[0]?.courseCode || classesAfterBreak[0]?.courseTitle) {
+                                                setCourseId(classesAfterBreak[0]?._id);
+                                                setRowIndex(classesAfterBreak[0]?.rowIndex);
+                                                document.getElementById("teacher_assign").showModal();
+                                            }
+                                        }}
+                                        className={`border border-black p-3`} colSpan={2}>
+                                        {`${classesAfterBreak[0]?.courseCode ?? ''} ${classesAfterBreak[0]?.courseTitle ? `(${classesAfterBreak[0]?.courseTitle})` : ''} ${classesAfterBreak[0]?.teacher?.sortForm ?? ''}`}
+                                    </td> : classesAfterBreak?.map((elem, ind) => <td key={ind}
+                                        onClick={() => {
+                                            if (elem?.courseCode || elem?.courseTitle) {
+                                                setCourseId(elem?._id);
+                                                setRowIndex(elem?.rowIndex);
+                                                document.getElementById("teacher_assign").showModal();
+                                            }
+                                        }}
+                                        className={`border border-black p-3`}>
                                         {`${elem?.courseCode ?? ''} ${elem?.courseTitle ? `(${elem?.courseTitle})` : ''} ${elem?.teacher?.sortForm ?? ''}`}
                                     </td>)
                                 }
@@ -57,6 +94,9 @@ const EveningTable = ({ data, loading }) => {
                     }
                 </tbody>
             </table>
+
+            {/* Teacher assign modal  */}
+            <TeacherAssign courseId={courseId} rowIndex={rowIndex} />
         </TableWrapper>
     );
 };

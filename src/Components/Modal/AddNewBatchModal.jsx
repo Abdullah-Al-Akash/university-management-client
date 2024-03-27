bec630fff8fb5aea34382d259648909da19799cb
ashiqur-r-biplop <biplops22@gmail.com>
2024-03-27 16:20:43 +0600

    added some code

--- src/Components/Modal/AddNewBatchModal.jsx
+++ src/Components/Modal/AddNewBatchModal.jsx
@@ -1,51 +1,83 @@
-
 import { useForm } from "react-hook-form";
 import { FaXmark } from "react-icons/fa6";
 
-
 const AddNewBatchModal = () => {
+  <button className="text-red-500 text-2xl" onClick={() => reset()}>
+    <FaXmark />
+  </button>;
+  const {
+    register,
+    handleSubmit,
+    reset,
+    watch,
+    formState: { errors },
+  } = useForm(); // initialize the hook
+  const addNewBatchHandler = (form) => {
+    const { confirm } = form;
+    if (confirm) {
+      fetch(
+        `https://routine-management-system-backend.onrender.com/api/routine/add-new-batch`,
+        {
+          method: "PATCH",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({}),
+        }
+      )
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+        });
+    }
+  };
 
-                        <button className="text-red-500 text-2xl" onClick={()=> reset()}><FaXmark /></button>
-    const { register, handleSubmit, reset, watch, formState:{errors} } = useForm(); // initialize the hook
-    const addNewBatchHandler = (form) => {
-        const {confirm} = form
-        console.log(confirm);
-    };
+  console.log(errors, 12);
 
-    console.log(errors, 12);
+  return (
+    <dialog id="add_new_batch_modal" className="modal">
+      <div className="modal-box">
+        <form className="space-y-4" onSubmit={handleSubmit(addNewBatchHandler)}>
+          <label htmlFor="confirm" className="font-semibold">
+            Please type <span className="font-bold">Confirm</span> to add new
+            batch
+          </label>
+          <div className="space-y-2">
+            <input
+              type="text"
+              placeholder="Confirm"
+              id="confirm"
+              {...register("confirm", {
+                validate: (value) =>
+                  value === "Confirm" || 'Value must be "Confirm"',
+              })}
+              className="my-inp"
+            />
+            {/* {errors.confirm?.message && <span className="text-red-500">*{errors.confirm?.message}</span>} */}
+          </div>
 
-    return (
-        <dialog id="add_new_batch_modal" className="modal">
-            <div className="modal-box">
+          <button
+            type="submit"
+            className={`my-btn-one ${
+              watch("confirm") != "Confirm" && "!opacity-35"
+            }`}
+            disabled={watch("confirm") != "Confirm"}
+          >
+            Submit
+          </button>
+        </form>
 
-                <form className="space-y-4" onSubmit={handleSubmit(addNewBatchHandler)}>
-                    <label htmlFor="confirm" className="font-semibold">Please type <span className="font-bold">Confirm</span> to add new batch</label>
-                    <div className="space-y-2">
-                        <input
-                            type="text"
-                            placeholder="Confirm"
-                            id="confirm"
-                            {...register('confirm', {
-                                validate: (value) => value === 'Confirm' || 'Value must be "Confirm"'
-                            })}
-                            className="my-inp"
-                        />
-                        {/* {errors.confirm?.message && <span className="text-red-500">*{errors.confirm?.message}</span>} */}
-                    </div>
-
-                    <button type="submit" className={`my-btn-one ${watch('confirm') != 'Confirm' && '!opacity-35'}`} disabled={watch('confirm')!= 'Confirm'}>Submit</button>
-                </form>
-
-
-                <div className="modal-action absolute top-0 right-1 mt-0">
-                    <form method="dialog">
-                        {/* if there is a button in form, it will close the modal */}
-                        <button className="text-red-500 text-2xl" onClick={()=> reset()}><FaXmark /></button>
-                    </form>
-                </div>
-            </div>
-        </dialog>
-    );
+        <div className="modal-action absolute top-0 right-1 mt-0">
+          <form method="dialog">
+            {/* if there is a button in form, it will close the modal */}
+            <button className="text-red-500 text-2xl" onClick={() => reset()}>
+              <FaXmark />
+            </button>
+          </form>
+        </div>
+      </div>
+    </dialog>
+  );
 };
 
 export default AddNewBatchModal;

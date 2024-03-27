d02ae12f4ac605d623bb6784d1e465d715cefe69
Source-Code-007 <utsho926@gmail.com>
2024-03-27 10:00:42 +0600

    updated structure added in evening routine

--- src/Components/Evening/EveningTable.jsx
+++ src/Components/Evening/EveningTable.jsx
@@ -30,25 +30,26 @@ const EveningTable = ({ data, loading }) => {
                             const { batch, yearSem, sem, room, courses } = elem
                             const classesBeforeBreak = courses.slice(0, 2)
                             const classesAfterBreak = courses.slice(2, 4)
+                            // console.log(elem);
                             return <tr key={ind} >
                                 <td className='border border-black p-3'>{batch}</td>
                                 <td className='border border-black p-3'>{yearSem}</td>
                                 <td className='border border-black p-3'>{sem}</td>
                                 <td className='border border-black p-3'>{room}</td>
                                 {
-                                    classesBeforeBreak[0]?.[1]?.courseCode === classesBeforeBreak[1]?.[2]?.courseCode ? <td className={`border border-black p-3`} colSpan={2}>
-                                        {`${classesBeforeBreak[0]?.[1]?.courseCode ?? ''} ${classesBeforeBreak[0]?.[1]?.courseTitle ? `(${classesBeforeBreak[0]?.[1]?.courseTitle})` : ''} ${classesBeforeBreak[0]?.[1]?.teacher?.sortForm ?? ''}`}
+                                    classesBeforeBreak[0]?.courseCode === classesBeforeBreak[1]?.courseCode ? <td className={`border border-black p-3`} colSpan={2}>
+                                        {`${classesBeforeBreak[0]?.courseCode ?? ''} ${classesBeforeBreak[0]?.courseTitle ? `(${classesBeforeBreak[0]?.courseTitle})` : ''} ${classesBeforeBreak[0]?.teacher?.sortForm ?? ''}`}
                                     </td> : classesBeforeBreak?.map((elem, ind) => <td key={ind} className={`border border-black p-3`}>
-                                        {`${elem[ind + 1]?.courseCode ?? ''} ${elem[ind + 1]?.courseTitle ? `(${elem[ind + 1]?.courseTitle})` : ''}  ${elem[ind + 1]?.teacher?.sortForm ?? ''}`}
+                                        {`${elem?.courseCode ?? ''} ${elem?.courseTitle ? `(${elem?.courseTitle})` : ''}  ${elem?.teacher?.sortForm ?? ''}`}
                                     </td>
                                     )
                                 }
                                 {ind === 0 && <td rowSpan={data.length}><div className='flex flex-col font-bold'>{"BREAK".split('').map((elem, ind) => <span key={ind}>{elem}</span>)}</div></td>}
                                 {
-                                    classesAfterBreak[0]?.[3]?.courseCode === classesAfterBreak[1]?.[4]?.courseCode ? <td className={`border border-black p-3`} colSpan={2}>
-                                        {`${classesBeforeBreak[0]?.[3]?.courseCode ?? ''} ${classesBeforeBreak[0]?.[3]?.courseTitle ? `(${classesBeforeBreak[0]?.[3]?.courseTitle})` : ''} ${classesBeforeBreak[0]?.[3]?.teacher?.sortForm ?? ''}`}
+                                    classesAfterBreak[0]?.courseCode === classesAfterBreak[1]?.courseCode ? <td className={`border border-black p-3`} colSpan={2}>
+                                        {`${classesBeforeBreak[0]?.courseCode ?? ''} ${classesBeforeBreak[0]?.courseTitle ? `(${classesBeforeBreak[0]?.courseTitle})` : ''} ${classesBeforeBreak[0]?.teacher?.sortForm ?? ''}`}
                                     </td> : classesAfterBreak?.map((elem, ind) => <td key={ind} className={`border border-black p-3`}>
-                                        {`${elem[ind + 3]?.courseCode ?? ''} ${elem[ind + 3]?.courseTitle ? `(${elem[ind + 3]?.courseTitle})` : ''} ${elem[ind + 3]?.teacher?.sortForm ?? ''}`}
+                                        {`${elem?.courseCode ?? ''} ${elem?.courseTitle ? `(${elem?.courseTitle})` : ''} ${elem?.teacher?.sortForm ?? ''}`}
                                     </td>)
                                 }
                             </tr>

97e5a07d4f69cffd866cf7738afab89b8ab26f5b
Source-Code-007 <utsho926@gmail.com>
2024-04-25 16:29:03 +0600

    insert faculty done

--- src/Components/Faculty/FacultyTable.jsx
+++ src/Components/Faculty/FacultyTable.jsx
@@ -6,6 +6,8 @@ import UploadFacultiesModal from "../Modal/UploadFacultiesModal";
 import Loading from "../../Shared/Loading";
 import { CgClose } from "react-icons/cg";
 import Swal from "sweetalert2";
+import InsertFaculty from "../Modal/InsertFaculty";
+import { ToastContainer } from "react-toastify";
 
 const FacultyTable = () => {
   const navigate = useNavigate();
@@ -61,11 +63,11 @@ const FacultyTable = () => {
           .catch((err) => {
             console.log(err);
           });
-        
+
       }
     });
 
-   
+
   };
   return (
     <div className="container mx-auto">
@@ -76,20 +78,39 @@ const FacultyTable = () => {
         >
           <FaBackward /> <span>back</span>
         </button>
-        <button
-          onClick={() => document.getElementById("uploadFaculties").showModal()}
-        >
-          <a
-            href="#_"
+        <div className="space-x-5">
+          {/* Upload faculties */}
+          <button
+            onClick={() => document.getElementById("uploadFaculties").showModal()}
+          >
+            <a
+              href="#_"
+              className="relative px-5 py-2 font-medium text-white group"
+            >
+              <span className="absolute inset-0 w-full h-full transition-all duration-300 ease-out transform translate-x-0 -skew-x-12 bg-orange-400 group-hover:bg-orange-500 group-hover:skew-x-12"></span>
+              <span className="absolute inset-0 w-full h-full transition-all duration-300 ease-out transform skew-x-12 bg-orange-500 group-hover:bg-orange-700 group-hover:-skew-x-12"></span>
+              <span className="absolute bottom-0 left-0 hidden w-10 h-20 transition-all duration-100 ease-out transform -translate-x-8 translate-y-10 bg-orange-500 -rotate-12"></span>
+              <span className="absolute bottom-0 right-0 hidden w-10 h-20 transition-all duration-100 ease-out transform translate-x-10 translate-y-8 bg-orange-500 -rotate-12"></span>
+              <span className="relative">Upload Faculties</span>
+            </a>
+          </button>
+
+          {/* Insert faculty */}
+          {/* Added new batch*/}
+          <button
+            onClick={() =>
+              document.getElementById("insertFacultyModal").showModal()
+            }
             className="relative px-5 py-2 font-medium text-white group"
           >
-            <span className="absolute inset-0 w-full h-full transition-all duration-300 ease-out transform translate-x-0 -skew-x-12 bg-orange-400 group-hover:bg-orange-500 group-hover:skew-x-12"></span>
-            <span className="absolute inset-0 w-full h-full transition-all duration-300 ease-out transform skew-x-12 bg-orange-500 group-hover:bg-orange-700 group-hover:-skew-x-12"></span>
-            <span className="absolute bottom-0 left-0 hidden w-10 h-20 transition-all duration-100 ease-out transform -translate-x-8 translate-y-10 bg-orange-500 -rotate-12"></span>
-            <span className="absolute bottom-0 right-0 hidden w-10 h-20 transition-all duration-100 ease-out transform translate-x-10 translate-y-8 bg-orange-500 -rotate-12"></span>
-            <span className="relative">Upload Faculties</span>
-          </a>
-        </button>
+            <span className="absolute inset-0 w-full h-full transition-all duration-300 ease-out transform translate-x-0 -skew-x-12 bg-purple-500 group-hover:bg-purple-700 group-hover:skew-x-12"></span>
+            <span className="absolute inset-0 w-full h-full transition-all duration-300 ease-out transform skew-x-12 bg-purple-700 group-hover:bg-purple-500 group-hover:-skew-x-12"></span>
+            <span className="absolute bottom-0 left-0 hidden w-10 h-20 transition-all duration-100 ease-out transform -translate-x-8 translate-y-10 bg-purple-600 -rotate-12"></span>
+            <span className="absolute bottom-0 right-0 hidden w-10 h-20 transition-all duration-100 ease-out transform translate-x-10 translate-y-8 bg-purple-400 -rotate-12"></span>
+            <span className="relative">Insert faculty</span>
+          </button>
+        </div>
+
       </div>
 
       <div className="text-end my-3">
@@ -137,6 +158,22 @@ const FacultyTable = () => {
       </div>
 
       <UploadFacultiesModal setControl={setControl} control={control} />
+      <InsertFaculty setControl={setControl} control={control} />
+
+
+      {/* Toast container */}
+      <ToastContainer
+        position="bottom-center"
+        autoClose={2000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
     </div>
   );
 };

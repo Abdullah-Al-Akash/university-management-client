8b2667d40be971649e3986e704f293a7b26caf42
ashiqur-r-biplop <biplops22@gmail.com>
2024-04-26 16:30:04 +0600

    possition change

--- src/Shared/SelectShiftBatch.jsx
+++ src/Shared/SelectShiftBatch.jsx
@@ -22,20 +22,6 @@ const SelectShiftBatch = ({ setSelectBatch, setSelectShift, selectShift }) => {
       </div>
 
       <div className="flex items-center gap-4">
-        {/* Added new batch*/}
-        <button
-          onClick={() =>
-            document.getElementById("add_new_batch_modal").showModal()
-          }
-          className="relative px-5 py-2 font-medium text-white group"
-        >
-          <span className="absolute inset-0 w-full h-full transition-all duration-300 ease-out transform translate-x-0 -skew-x-12 bg-purple-500 group-hover:bg-purple-700 group-hover:skew-x-12"></span>
-          <span className="absolute inset-0 w-full h-full transition-all duration-300 ease-out transform skew-x-12 bg-purple-700 group-hover:bg-purple-500 group-hover:-skew-x-12"></span>
-          <span className="absolute bottom-0 left-0 hidden w-10 h-20 transition-all duration-100 ease-out transform -translate-x-8 translate-y-10 bg-purple-600 -rotate-12"></span>
-          <span className="absolute bottom-0 right-0 hidden w-10 h-20 transition-all duration-100 ease-out transform translate-x-10 translate-y-8 bg-purple-400 -rotate-12"></span>
-          <span className="relative">Add new batch</span>
-        </button>
-
         {/* All faculty */}
         <Link to="/all-faculties">
           <button>
@@ -52,9 +38,22 @@ const SelectShiftBatch = ({ setSelectBatch, setSelectShift, selectShift }) => {
             </a>
           </button>
         </Link>
+        {/* Added new batch*/}
+        <button
+          onClick={() =>
+            document.getElementById("add_new_batch_modal").showModal()
+          }
+          className="relative px-5 py-2 font-medium text-white group"
+        >
+          <span className="absolute inset-0 w-full h-full transition-all duration-300 ease-out transform translate-x-0 -skew-x-12 bg-purple-500 group-hover:bg-purple-700 group-hover:skew-x-12"></span>
+          <span className="absolute inset-0 w-full h-full transition-all duration-300 ease-out transform skew-x-12 bg-purple-700 group-hover:bg-purple-500 group-hover:-skew-x-12"></span>
+          <span className="absolute bottom-0 left-0 hidden w-10 h-20 transition-all duration-100 ease-out transform -translate-x-8 translate-y-10 bg-purple-600 -rotate-12"></span>
+          <span className="absolute bottom-0 right-0 hidden w-10 h-20 transition-all duration-100 ease-out transform translate-x-10 translate-y-8 bg-purple-400 -rotate-12"></span>
+          <span className="relative">Add new batch</span>
+        </button>
 
         {/* Upload routine */}
-        <button
+        {/* <button
           onClick={() => document.getElementById("uploadRoutine").showModal()}
           className="relative px-5 py-2 font-medium text-white group"
         >
@@ -63,7 +62,7 @@ const SelectShiftBatch = ({ setSelectBatch, setSelectShift, selectShift }) => {
           <span className="absolute bottom-0 left-0 hidden w-10 h-20 transition-all duration-100 ease-out transform -translate-x-8 translate-y-10 bg-purple-600 -rotate-12"></span>
           <span className="absolute bottom-0 right-0 hidden w-10 h-20 transition-all duration-100 ease-out transform translate-x-10 translate-y-8 bg-purple-400 -rotate-12"></span>
           <span className="relative">Upload Routine</span>
-        </button>
+        </button> */}
 
         {/* Insert routine */}
         <button
@@ -93,7 +92,6 @@ const SelectShiftBatch = ({ setSelectBatch, setSelectShift, selectShift }) => {
           <span className="absolute bottom-0 right-0 hidden w-10 h-20 transition-all duration-100 ease-out transform translate-x-10 translate-y-8 bg-purple-400 -rotate-12"></span>
           <span className="relative">Insert course</span>
         </button>
-
       </div>
 
       {/* Select batch */}
